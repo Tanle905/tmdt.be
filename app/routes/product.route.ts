6dc24e38135c60b78da4d87625666b04f3ea53f2
Tan Le <tan.tran@siliconstack.com.au>
2022-10-09 01:29:23 +0700

    fix delete not need token

--- app/routes/product.route.ts
+++ app/routes/product.route.ts
@@ -4,22 +4,18 @@ import { productController } from "../controller/product.controller";
 import { authJwt } from "../middleware/authJwt";
 
 export const productRouter = Router();
+productRouter.route(PRODUCT_ROUTE.BASE).get(productController.get);
+
+productRouter.route(PRODUCT_ROUTE.ID).get(productController.getById);
+
 productRouter
   .route(PRODUCT_ROUTE.BASE)
-  .get(productController.get)
+  .all(authJwt.verifyToken, authJwt.isAdmin)
+  .post(productController.post)
   .delete(productController.delete);
 
 productRouter
   .route(PRODUCT_ROUTE.ID)
-  .get(productController.getById)
+  .all(authJwt.verifyToken, authJwt.isAdmin)
+  .put(productController.put)
   .delete(productController.deleteById);
-
-productRouter
-  .route(PRODUCT_ROUTE.BASE)
-  .all(authJwt.verifyToken, authJwt.isAdmin)
-  .post(productController.post);
-
-productRouter
-  .route(PRODUCT_ROUTE.ID)
-  .all(authJwt.verifyToken, authJwt.isAdmin)
-  .put(productController.put);
