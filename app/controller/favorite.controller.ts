cfe74caa879b3d7d155589827b25e256d0d505e7
Tan Le <tan.tran@siliconstack.com.au>
2022-10-15 00:07:35 +0700

    done favorite

--- app/controller/favorite.controller.ts
+++ app/controller/favorite.controller.ts
@@ -1,14 +1,26 @@
 import { Response } from "express";
 import { FavoriteRequest } from "../interface/favorite.interface";
 import { FavoriteModel } from "../model/favorite.model";
+import { ProductModel } from "../model/product.model";
 
 export const favoriteController = {
   get: async (req: FavoriteRequest, res: Response) => {
     const { userId } = res.locals;
     try {
       const document = await FavoriteModel.findOne({ userId });
-      return res.status(200).json({ data: document || [] });
-    } catch (error) {
+      const mappedProductsList = document
+      ? await Promise.all(
+          document.productsList.map(async (item: any) => {
+            const product = await ProductModel.findOne({
+              _id: item,
+            });
+            return {
+              ...product?.toObject(),
+            };
+          })
+        )
+      : [];
+    return res.json({ data: mappedProductsList });    } catch (error) {
       res.status(500).json({ message: error });
     }
   },
@@ -20,22 +32,25 @@ export const favoriteController = {
       if (userId) {
         const currentFavoriteList = await FavoriteModel.findOne({ userId });
         if (currentFavoriteList) {
-          const existedProductInFavorite = currentFavoriteList.productsList.filter(
-            (item: String) => item === requestFavorite
-          )[0];
+          const existedProductInFavorite =
+            currentFavoriteList.productsList.filter(
+              (item: String) => item === requestFavorite
+            )[0];
           if (existedProductInFavorite) {
             await FavoriteModel.findOneAndUpdate(
               {
                 _id: currentFavoriteList._id,
-                "productsList": existedProductInFavorite,
+                productsList: existedProductInFavorite,
               },
               {
                 $pullAll: {
-                  "productsList": [existedProductInFavorite],
+                  productsList: [existedProductInFavorite],
                 },
               }
             );
-            return res.json({ message: "Product has been removed from favorite!" });
+            return res.json({
+              message: "Product has been removed from favorite!",
+            });
           } else {
             currentFavoriteList.productsList.push(requestFavorite);
           }
