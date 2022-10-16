a3b973c926f5f8bb50c43a238ac2f06a05ecb546
Tan Le <tan.tran@siliconstack.com.au>
2022-10-16 20:18:56 +0700

    added favorite to cart

--- app/controller/cart.controller.ts
+++ app/controller/cart.controller.ts
@@ -2,12 +2,15 @@ import { Response } from "express";
 import { ObjectId } from "mongodb";
 import { CartRequest } from "../interface/cart.interface";
 import { CartModel } from "../model/cart.model";
+import { FavoriteModel } from "../model/favorite.model";
 import { ProductModel } from "../model/product.model";
 
 export const cartController = {
   getById: async (req: CartRequest, res: Response) => {
     const { userId } = res.locals;
     try {
+      let favoriteProductsList;
+
       if (userId) {
         const document = await CartModel.find({ userId });
         const mappedProductsList = document[0]
@@ -23,7 +26,17 @@ export const cartController = {
               })
             )
           : [];
-        return res.json({ data: mappedProductsList });
+        const currentFavoriteList = await FavoriteModel.findOne({ userId });
+        if (currentFavoriteList) {
+          favoriteProductsList = mappedProductsList.map((product) => {
+            if (currentFavoriteList.productsList.includes(product._id))
+              return { ...product, isFavorite: true };
+            else return product;
+          });
+        }
+        return res.json({
+          data: currentFavoriteList ? favoriteProductsList : mappedProductsList,
+        });
       }
     } catch (error) {
       return res.json({ message: error });
@@ -74,6 +87,7 @@ export const cartController = {
       return res.json({ message: error });
     }
   },
+  updateCart: async (req: CartRequest, res: Response) => {},
   deleteProductInCart: async (req: CartRequest, res: Response) => {
     const { userId } = res.locals;
     const requestProduct = req.body.data[0];
