18e1f3f1e5729567a4f2aaaab00c75bd3c21345d
Tan Le <tan.tran@siliconstack.com.au>
2022-10-23 10:14:36 +0700

    update timestamp for order

--- app/model/order.model.ts
+++ app/model/order.model.ts
@@ -3,30 +3,33 @@ import { OrderDetails } from "../interface/order.interface";
 import { addressDataSchema } from "./address.model";
 import { productDataSchema } from "./product.model";
 
-export const orderDataSchema: Schema = new mongoose.Schema<OrderDetails>({
-  userId: {
-    type: String,
-    required: true,
+export const orderDataSchema: Schema = new mongoose.Schema<OrderDetails>(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    items: [productDataSchema],
+    address: addressDataSchema,
+    paymentStatus: {
+      type: String,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
   },
-  orderStatus: {
-    type: String,
-    required: true,
-  },
-  currency: {
-    type: String,
-    required: true,
-  },
-  items: [productDataSchema],
-  address: addressDataSchema,
-  paymentStatus: {
-    type: String,
-    required: true,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
 
 export const OrderModel = mongoose.model<OrderDetails>(
   "Order",
